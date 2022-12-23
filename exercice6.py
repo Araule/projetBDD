251f34291ec1c938fee4600c20c79582b315e135
Camille CLAVIER <camilleclavier9@gmail.com>
2022-12-23 10:38:14 +0100

    update exercice6

--- exercice6.py
+++ exercice6.py
@@ -8,31 +8,56 @@ import csv
 bdd = sqlite3.connect("BARS.db")
 curseur = bdd.cursor()
 
-def affiche(requete):
-    curseur.execute(requete)
-    results = curseur.fetchall()
-    for r in results:
-        print(f"{r}")
-    return 
+#connexion du manager
+identifiant = input("\nEntrez votre identifiant : ")
+mot_de_passe = input("Entrez votre matricule : ")
 
-#Donnez à chaque manager la possibilité d’afficher le nombre de ventes effectuées ce mois-ci
-#par ses employés et le montant que cela représente, ainsi que le bénéfice généré pour chaque
-#employé du bar.
+connexion = f"SELECT identifiant \
+                FROM Managers \
+                WHERE identifiant = '{identifiant}' \
+                AND matricule = '{mot_de_passe}';"
+curseur.execute(connexion)
+if not curseur.fetchone() :  
+    print("\nIdentifiants incorrects.")
+    exit() 
+else :
+    print("\nVous souhaitez connaître le nombre de boissons vendues dans votre établissement.\n")
+
+curseur.execute(f"SELECT nom_bar \
+                    FROM Etablissements \
+                    WHERE matricule_manager = '{mot_de_passe}'")
+nom_bar = curseur.fetchone()
+
+print(f"Vous êtes le manager du bar \"{nom_bar[0]}\".\n")
+
+'''
 dates = []
 curseur.execute("SELECT strftime('%m',date) FROM Ventes;")
 for ligne in curseur.fetchall():
     dates.append(ligne[0])
 print(dates)
+'''
 
 #problèmes : 
 # faire entrer que le mois à l'utilisateur
 #ne sait pas comment faire pour aussi afficher le bénéfice généré pour chaque employé dans la même requête
 
-nb_ventes_mois = "SELECT COUNT(Ventes.idBoisson), ROUND(SUM(Carte.prix_EU),2), Etablissements.nom_bar\
-    FROM Ventes, Carte, Etablissements, Employes\
-        WHERE Carte.idBoisson = Ventes.idBoisson\
-            AND Etablissements.nom_bar = Employes.nom_bar\
-                AND Ventes.matricule = Employes.matricule\
-                    AND Ventes.date BETWEEN '01-11-2022' AND '31-11-2022'\
-                    GROUP BY Etablissements.nom_bar "
-affiche(nb_ventes_mois)
+curseur.execute(f"SELECT COUNT(V.idBoisson), ROUND(SUM(C.prix_EU),2)\
+    FROM Ventes AS V, Carte AS C , Employes AS E, Etablissements AS Et\
+        WHERE C.idBoisson = V.idBoisson\
+            AND E.nom_bar = \"{nom_bar[0]}\"\
+                AND Et.nom_bar = \"{nom_bar[0]}\"\
+                AND V.matricule = E.matricule\
+                    AND V.date BETWEEN '01-11-2022' AND '31-11-2022'\
+                        GROUP BY  Et.nom_bar")
+results = curseur.fetchall()
+for r in results :
+    print(f"Vos employés ont vendu {r[0]} boissons pour un total de {r[1]} euros.")
+
+#Bien préciser pour E.nom_bar et Et.nom_bar qu'il s'agit du bar du manager
+#Sinon problème de résultat
+#Exemple avec Daniel.Faviet / T80612 / Le Saphir
+#Si on ne met que E.nom_bar =\"{nom_bar[0]}\" ou que Et.nom_bar = \"{nom_bar[0]}\"\
+#On aura comme boisson vendu 50170 au lieu de 5017 et comme total 266985,0 au lieu de 26698.5
+
+bdd.close()
