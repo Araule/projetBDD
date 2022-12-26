c80e8cc72601148b86e301e607973b0c8fb6d156
Araule <lau7tsu@gmail.com>
2022-12-26 18:59:32 +0100

    update Laura

--- exercice12idee.py
+++ exercice12idee.py
@@ -39,108 +39,66 @@ nom_bar = curseur.fetchone()
 print(f"\nVous êtes le manager du bar \"{nom_bar[0]}\".")
 
 
-# On attache les 6 requêtes suivantes à des variables
-
-# affiche la boisson qui a rapporté le moins d'argent à la boisson qui a rapporté le plus d'argent
-moins_bénéfice = f"SELECT C.boisson, ROUND(SUM(C.prix_EU), 2) \
+# On rattache les résultats des deux requêtes à une variable pour les réutiliser plus tard
+curseur.execute(f"SELECT ROUND(SUM(C.prix_EU), 2) \
                     FROM Employes AS E \
                     INNER JOIN Ventes AS V \
                     ON E.matricule = V.matricule \
                     INNER JOIN Carte AS C \
                     ON C.idBoisson = V.idBoisson \
-                    WHERE E.nom_bar = \"{nom_bar[0]}\" \
-                    GROUP BY C.boisson \
-                    ORDER BY ROUND(SUM(C.prix_EU), 2)"
-
-# affiche le bénéfice moyen d'une boisson
-moyenne_bénéfice = f"SELECT ROUND(AVG(bénéfice), 2) \
-                        FROM (SELECT SUM(C.prix_EU) AS bénéfice \
-                                FROM Employes AS E \
-                                INNER JOIN Ventes AS V \
-                                ON E.matricule = V.matricule \
-                                INNER JOIN Carte AS C \
-                                ON C.idBoisson = V.idBoisson \
-                                WHERE E.nom_bar = \"{nom_bar[0]}\" \
-                                GROUP BY C.boisson \
-                                ORDER BY ROUND(SUM(C.prix_EU), 2))"
-
-# affiche le total des bénéfices
-total_bénéfice = f"SELECT ROUND(SUM(C.prix_EU), 2) \
-                    FROM Employes AS E \
-                    INNER JOIN Ventes AS V \
-                    ON E.matricule = V.matricule \
-                    INNER JOIN Carte AS C \
-                    ON C.idBoisson = V.idBoisson \
-                    WHERE E.nom_bar = \"{nom_bar[0]}\""
-
-
-# affiche la boisson la moins vendue à la boisson la plus vendue
-moins_vendue = f"SELECT C.boisson, COUNT(V.idboisson) \
-                    FROM Employes AS E \
-                    INNER JOIN Ventes AS V \
-                    ON E.matricule = V.matricule \
-                    INNER JOIN Carte AS C \
-                    ON C.idBoisson = V.idBoisson \
-                    WHERE E.nom_bar = \"{nom_bar[0]}\" \
-                    GROUP BY C.boisson \
-                    ORDER BY COUNT(V.idboisson)"
-
-# affiche le nombre de fois qu'a été vendue une boisson en moyenne
-moyenne_vendue = f"SELECT ROUND(AVG(vendue), 2) \
-                    FROM (SELECT COUNT(V.idboisson) AS vendue \
-                    FROM Employes AS E \
-                    INNER JOIN Ventes AS V \
-                    ON E.matricule = V.matricule \
-                    INNER JOIN Carte AS C \
-                    ON C.idBoisson = V.idBoisson \
-                    WHERE E.nom_bar = \"{nom_bar[0]}\" \
-                    GROUP BY C.boisson \
-                    ORDER BY COUNT(V.idboisson))"
-
-# nombre total de boissons vendues
-total_vendue = f"SELECT COUNT(V.idBoisson) \
-                    FROM Employes AS E \
-                    INNER JOIN Ventes AS V \
-                    ON E.matricule = V.matricule \
-                    INNER JOIN Carte AS C \
-                    ON C.idBoisson = V.idBoisson \
-                    WHERE E.nom_bar = \"{nom_bar[0]}\""
-
-# On rattache les résultats à la variable de la requête car nous n'en avons plus besoin
-curseur.execute(moyenne_bénéfice)
-results = curseur.fetchall()
-for r in results :
-    moyenne_bénéfice = r[0]
-    print(f"\nUne boisson a fait un bénéfice moyen de {moyenne_bénéfice} euros.")
-
-curseur.execute(total_bénéfice)
+                    WHERE E.nom_bar = \"{nom_bar[0]}\"") # total des bénéfices du mois de novembre dans le bar du manager
 results = curseur.fetchall()
 for r in results :
     total_bénéfice = r[0]
-    print(f"\nIl y a eu {total_bénéfice} euros de bénéfice total.")  
 
-curseur.execute(moyenne_vendue)
-results = curseur.fetchall()
-for r in results :
-    moyenne_vendue = r[0]
-    print(f"\nune boisson a été vendue {moyenne_vendue} fois en moyenne.")
-
-curseur.execute(total_vendue)
+curseur.execute(f"SELECT COUNT(V.idBoisson) \
+                    FROM Employes AS E \
+                    INNER JOIN Ventes AS V \
+                    ON E.matricule = V.matricule \
+                    INNER JOIN Carte AS C \
+                    ON C.idBoisson = V.idBoisson \
+                    WHERE E.nom_bar = \"{nom_bar[0]}\"") # nombre total de boissons vendues au mois de novembre dans le bar du manager
 results = curseur.fetchall()
 for r in results :
     total_vendue = r[0]
-    print(f"\nIl y a eu {total_vendue} boissons vendues.")
 
 
-# On affiche la liste des boissons en fonction de leur bénéfice et du nombre de fois qu'elles ont été vendues
-print("\nbénéfice par boisson : ")
-curseur.execute(moins_bénéfice)
+# On crée une liste vide qui contiendra le nom des boissons que le manager peut supprimer de la carte
+liste_boisson = []
+# Si une boisson représente moins de 1% des bénéfices du bar et 1% du nombre des boissons vendus par le bar
+# Alors le manager peut l'enlever de la carte
+
+
+# affiche la boisson qui a rapporté le moins d'argent à la boisson qui a rapporté le plus d'argent
+i = 0 # on initialise un compteur
+curseur.execute(f"SELECT C.boisson, ROUND(SUM(C.prix_EU), 2) \
+                    FROM Employes AS E \
+                    INNER JOIN Ventes AS V \
+                    ON E.matricule = V.matricule \
+                    INNER JOIN Carte AS C \
+                    ON C.idBoisson = V.idBoisson \
+                    WHERE E.nom_bar = \"{nom_bar[0]}\" \
+                    GROUP BY C.boisson \
+                    ORDER BY ROUND(SUM(C.prix_EU), 2)")
 results = curseur.fetchall()
 for r in results :
-    print(f"{r[0]} : {r[1]} euros")
+    if r[1] < (total_bénéfice * 0.01) :
+        curseur.execute(f"SELECT COUNT(V.idboisson) \
+                            FROM Employes AS E \
+                            INNER JOIN Ventes AS V \
+                            ON E.matricule = V.matricule \
+                            INNER JOIN Carte AS C \
+                            ON C.idBoisson = V.idBoisson \
+                            WHERE E.nom_bar = \"{nom_bar[0]}\" \
+                            AND C.boisson = \"{r[0]}\"")
+        results2 = curseur.fetchall()
+        for r2 in results2 :
+            if r2[0] < (total_vendue * 0.01) :
+                liste_boisson.append(f"{r[0]} : {r[1]} euros de bénéfice, vendue {r2[0]} fois")
+                i += 1
 
-print("\nnombre de vente par boisson : ")
-curseur.execute(moins_vendue)
-results = curseur.fetchall()
-for r in results :
-    print(f"{r[0]} : vendue {r[1]} fois.")
+print(f"\nVous pouvez supprimer de la carte {i} boissons.")
+for ligne in liste_boisson :
+    print(ligne)
+
+bdd.close()
